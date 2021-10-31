a11d9bf4e05426642047996b202d67a28e624c50
Lovish-SD <lovishk.prajapati@storydigital.in>
2021-10-31 20:32:39 +0530

    edit fixed

--- src/New.js
+++ src/New.js
@@ -1,60 +1,54 @@
-
-import React,{useState} from 'react';
-import './App.css'
-
+import React, { useState } from 'react';
+import './App.css';
 
 const New = () => {
+	const [Tod, setTod] = useState('');
+	const [Tods, setTods] = useState([]);
 
-const [Tod, setTod] = useState("");
-const [Tods, setTods] = useState([]);
+	const handleS = (e) => {
+		e.preventDefault();
 
-const handleS = (e)=>{
-    e.preventDefault();
+		if (Tod !== '') {
+			setTods([{ id: `${Tod}-${Date.now()}`, Tod }, ...Tods]);
+			setTod(' ');
+			return;
+		}
+	};
 
-    if(Tod !== '')
-    {
-        setTods([{id:`${Tod}-${Date.now()}`, Tod}, ...Tods]);
-        setTod(" ");
-        return;
-    }
-    
-};
+	const handleD = (id) => {
+		const dels = Tods.filter((to) => to.id !== id);
+		setTods([...dels]);
+	};
 
-const handleD = (id) => {
-        const dels = Tods.filter( (to)=> to.id !== id);
-        setTods([...dels]);
-};
+	const handleE = (id) => {
+		const eds = Tods.find((u) => u.id === id);
+		setTod(eds.id);
+	};
 
-
-const handleE = (id) =>{
-    const eds =  Tods.find((u)=> u.id === id);
-    setTod(eds.id);
-    
-};
-
-
-    return (
-        <div className="New">
-            <h1>TO DO LIST</h1>
-            <form className="TO" onSubmit={handleS}>
-                <input type ='text' value={Tod} onChange={(e)=> setTod(e.target.value)}/>
-                <button className="MainB" type="submit" >GO</button>
-                <ul className="Allto">
-                    {Tods.map((t) => (
-                        <li className="SingleT"  key={t.id}>
-                        <span  className="TEXT" >{t.Tod}</span>
-                        <button onClick={()=>handleE(t.id)}>EDIT</button>
-                        <button onClick={()=>handleD(t.id)} >DELETE</button>
-                    
-                    </li>
-                            
-                    ))}
-                    
-                </ul>
-
-            </form>
-        </div>
-    );
+	return (
+		<div className="New">
+			<h1>TO DO LIST</h1>
+			<form className="TO" onSubmit={handleS}>
+				<input
+					type="text"
+					value={Tod}
+					onChange={(e) => setTod(e.target.value)}
+				/>
+				<button className="MainB" type="submit">
+					GO
+				</button>
+			</form>
+			<ul className="Allto">
+				{Tods.map((t) => (
+					<li className="SingleT" key={t.id}>
+						<span className="TEXT">{t.Tod}</span>
+						<button onClick={() => handleE(t.id)}>EDIT</button>
+						<button onClick={() => handleD(t.id)}>DELETE</button>
+					</li>
+				))}
+			</ul>
+		</div>
+	);
 };
 
 export default New;
